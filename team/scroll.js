90128b3e7f750ef3ffaf45566dfa95edf26afc31
Shivendra Saurav <shivendrasaurav@gmail.com>
2020-02-09 18:55:58 +0530

    Fixed Veritical Scroll In Team

--- team/scroll.js
+++ team/scroll.js
@@ -1,8 +1,12 @@
 function scrleft(){
     var a = document.getElementById("scroll");
     var b = event.deltaY;
-    if(b > 0)
+    if(b > 0){
         a.scrollLeft +=250;
-    else
+        a.scrollTop += 0;
+    }
+    else{
         a.scrollLeft -=250;
+        a.scrollTop += 0;
+    }
 }
