926effb5f3bd73b7965478c6ca77a65941d9ffa1
Shivendra Saurav <shivendrasaurav@gmail.com>
2020-03-05 21:45:50 +0530

    updateva

---
+++ speakers/on_screen_window.js
@@ -0,0 +1,72 @@
+function openmod1(){
+    document.getElementById("mod1").style.display="Block";
+    document.getElementById("body").style.filter="saturate(250%) blur(25px)";
+}
+function closemod1(){
+    document.getElementById("mod1").style.display="None";
+    document.getElementById("body").style.filter="blur(0px)";
+}
+function openmod2(){
+    document.getElementById("mod1").style.display="Block";
+    document.getElementById("body").style.filter="saturate(250%) blur(25px)";
+}
+function closemod2(){
+    document.getElementById("mod2").style.display="None";
+    document.getElementById("body").style.filter="blur(0px)";
+}
+function openmod3(){
+    document.getElementById("mod3").style.display="Block";
+    document.getElementById("body").style.filter="saturate(250%) blur(25px)";
+}
+function closemod3(){
+    document.getElementById("mod3").style.display="None";
+    document.getElementById("body").style.filter="blur(0px)";
+}
+function openmod4(){
+    document.getElementById("mod4").style.display="Block";
+    document.getElementById("body").style.filter="saturate(250%) blur(25px)";
+}
+function closemod4(){
+    document.getElementById("mod4").style.display="None";
+    document.getElementById("body").style.filter="blur(0px)";
+}
+function openmod5(){
+    document.getElementById("mod5").style.display="Block";
+    document.getElementById("body").style.filter="saturate(250%) blur(25px)";
+}
+function closemod5(){
+    document.getElementById("mod5").style.display="None";
+    document.getElementById("body").style.filter="blur(0px)";
+}
+function openmod6(){
+    document.getElementById("mod6").style.display="Block";
+    document.getElementById("body").style.filter="saturate(250%) blur(25px)";
+}
+function closemod6(){
+    document.getElementById("mod6").style.display="None";
+    document.getElementById("body").style.filter="blur(0px)";
+}
+function openmod7(){
+    document.getElementById("mod7").style.display="Block";
+    document.getElementById("body").style.filter="saturate(250%) blur(25px)";
+}
+function closemod7(){
+    document.getElementById("mod7").style.display="None";
+    document.getElementById("body").style.filter="blur(0px)";
+}
+function openmod8(){
+    document.getElementById("mod8").style.display="Block";
+    document.getElementById("body").style.filter="saturate(250%) blur(25px)";
+}
+function closemod8(){
+    document.getElementById("mod8").style.display="None";
+    document.getElementById("body").style.filter="blur(0px)";
+}
+function openmod9(){
+    document.getElementById("mod9").style.display="Block";
+    document.getElementById("body").style.filter="saturate(250%) blur(25px)";
+}
+function closemod9(){
+    document.getElementById("mod9").style.display="None";
+    document.getElementById("body").style.filter="blur(0px)";
+}
---
+++ speakers/spkslide.js
@@ -0,0 +1,32 @@
+function showslide1(){
+    document.getElementById("slide1").style.display=("block");
+    document.getElementById("slide2").style.display=("none");
+    document.getElementById("slide3").style.display=("none");
+    document.getElementById("slide4").style.display=("none");
+    document.getElementById("slide5").style.display=("none");
+    document.getElementById("slide6").style.display=("none");
+    document.getElementById("slide7").style.display=("none");
+    document.getElementById("slide8").style.display=("none");
+}
+
+function showslide2(){
+    document.getElementById("slide1").style.display=("none");
+    document.getElementById("slide2").style.display=("block");
+    document.getElementById("slide3").style.display=("none");
+    document.getElementById("slide4").style.display=("none");
+    document.getElementById("slide5").style.display=("none");
+    document.getElementById("slide6").style.display=("none");
+    document.getElementById("slide7").style.display=("none");
+    document.getElementById("slide8").style.display=("none");
+}
+
+function showslide3(){
+    document.getElementById("slide1").style.display=("none");
+    document.getElementById("slide2").style.display=("none");
+    document.getElementById("slide3").style.display=("block");
+    document.getElementById("slide4").style.display=("none");
+    document.getElementById("slide5").style.display=("none");
+    document.getElementById("slide6").style.display=("none");
+    document.getElementById("slide7").style.display=("none");
+    document.getElementById("slide8").style.display=("none");
+}
