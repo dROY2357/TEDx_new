d9d7f3e9e81165197930efc339a4980a7942db52
Shivendra Saurav <shivendrasaurav@gmail.com>
2020-03-06 12:35:03 +0530

    Fixed Footers And SidePanels

--- team/on_screen_window.js
+++ team/on_screen_window.js
@@ -7,7 +7,7 @@ function closemod1(){
     document.getElementById("body").style.filter="blur(0px)";
 }
 function openmod2(){
-    document.getElementById("mod1").style.display="Block";
+    document.getElementById("mod2").style.display="Block";
     document.getElementById("body").style.filter="saturate(250%) blur(25px)";
 }
 function closemod2(){
